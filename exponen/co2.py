0c10bedd65925891dadaa732a1c5a5b01beba5ee
YogaNovvaindra <yoga.november2000@gmail.com>
2023-11-04 18:06:44 +0700

    change using SQLAlchemy

--- exponen/co2.py
+++ exponen/co2.py
@@ -1,22 +1,26 @@
 import os
 import pandas as pd
 import pymysql
+from sqlalchemy import create_engine
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_forecast_co2(esp_id):
-    connection = pymysql.connect(
-        host=os.getenv("MYSQL_HOST"),
-        user=os.getenv("MYSQL_USER"),
-        password=os.getenv("MYSQL_PASSWORD"),
-        database=os.getenv("MYSQL_DB")
-    )
+    # connection = pymysql.connect(
+    #     host=os.getenv("MYSQL_HOST"),
+    #     user=os.getenv("MYSQL_USER"),
+    #     password=os.getenv("MYSQL_PASSWORD"),
+    #     database=os.getenv("MYSQL_DB")
+    # )
+
+    db_uri = f"mysql+mysqlconnector://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT')}/{os.getenv('MYSQL_DB')}"
+    engine = create_engine(db_uri)
 
     query = "SELECT mq135 FROM dummy"
     # query = f"SELECT mq135 FROM dummy WHERE esp_id = '{esp_id}' ORDER BY timestamp DESC"
-    df = pd.read_sql(query, connection)
+    df = pd.read_sql(query, engine)
 
     time_series = df['mq135']
 
@@ -42,5 +46,4 @@ def get_forecast_co2(esp_id):
     forecast_period = 12
     forecast = model_fit.forecast(steps=forecast_period)
 
-    connection.close()
     return forecast.tolist()
