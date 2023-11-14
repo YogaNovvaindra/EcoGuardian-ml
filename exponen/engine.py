c30fc9974158202f8d178c09bdeec2a8ebd0805b
YogaNovvaindra <yoga.november2000@gmail.com>
2023-11-14 21:07:17 +0700

    exponen + 2h

--- exponen/engine.py
+++ exponen/engine.py
@@ -9,6 +9,7 @@ def triple_exponential_smoothing(data, seasonality_period, alpha, beta, gamma, f
     # Inisialisasi variabel hasil
     fitted_values = []
     forecast_values = []
+    # round forecast value to 3 decimal places and make it float
 
     for i in range(len(data) + forecast_period):
         # Update level, trend, dan seasonal
@@ -30,4 +31,6 @@ def triple_exponential_smoothing(data, seasonality_period, alpha, beta, gamma, f
             trend = beta * (level - last_level) + (1 - beta) * last_trend
             seasonal[i % seasonality_period] = gamma * (forecast_values[-1] - level) + (1 - gamma) * seasonal[i % seasonality_period]
 
+    forecast_values = [round(float(i), 3) for i in forecast_values]
+    
     return forecast_values
